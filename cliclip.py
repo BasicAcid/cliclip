2fc54082ca32481177e44b8a5440f0175f011ca4
BasicAcid <david.tabarie@gmail.com>
2023-12-21 21:57:06 +0100

    Satisfy pylint to pass the class.

--- cliclip.py
+++ cliclip.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+Find local images based on text prompt.
+"""
+
 import os
 import argparse
 import PIL.Image
@@ -5,22 +11,28 @@ import torch
 from tqdm import tqdm
 import clip
 
-def main():
-    parser = argparse.ArgumentParser(description="Search for images based on textual prompts using CLIP.")
-    parser.add_argument("--directory", type=str, required=True, help="Directory containing the images.")
-    parser.add_argument("--prompt", type=str, required=True, help="Text prompt.")
-    parser.add_argument("--top", type=int, default=15, help="Number of top scored images to display.")
-    args = parser.parse_args()
+# Use Cuda if available.
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load('ViT-B/32', DEVICE)
 
-    # Use Cuda if available.
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load('ViT-B/32', DEVICE)
+def main():
+    """Main"""
+    parser = argparse.ArgumentParser(
+        description="Search for images based on textual prompts using CLIP.")
+    parser.add_argument("--directory", type=str, required=True,
+                        help="Directory containing the images.")
+    parser.add_argument("--prompt", type=str, required=True, help="Text prompt.")
+    parser.add_argument("--top", type=int, default=15,
+                        help="Number of top scored images to display.")
+    args = parser.parse_args()
 
     # Encode text prompt.
     text_encoded = clip.tokenize(args.prompt).to(DEVICE)
     text_features = model.encode_text(text_encoded)
 
-    image_filenames = [f for f in os.listdir(args.directory) if os.path.isfile(os.path.join(args.directory, f)) and f.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
+    image_filenames = [f for f in os.listdir(args.directory)
+                       if os.path.isfile(os.path.join(args.directory, f))
+                       and f.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
     image_scores = []
 
     for image_filename in tqdm(image_filenames, desc="Processing images"):
@@ -43,8 +55,8 @@ def main():
     sorted_images = sorted(image_scores, key=lambda x: x[1], reverse=True)
 
     # Display the top scored images along with their full paths.
-    n = min(args.top, len(sorted_images))
-    for i, (image_path, score) in enumerate(sorted_images[:n]):
+    top_scored = min(args.top, len(sorted_images))
+    for i, (image_path, score) in enumerate(sorted_images[:top_scored]):
         print(f"Rank {i+1}: {image_path} (score: {score:.2f})")
 
 
